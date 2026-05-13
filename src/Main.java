06998bcacb200dc29e834cd1fc705405fa3e59a6
DanielaThais <daniela.thais64@gmail.com>
2026-05-12 21:12:20 -0300

    Entendendo a diferença de performance entre String concatenada,StringBuilder e String Buffer (single e multithreads)

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
-import java.util.TreeMap;
-import java.util.Map;
-import java.util.HashMap;
+import java.time.Duration;
+import java.time.OffsetDateTime;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		var value = """
-				{"name":"João","age":30,
-				""";
+		var stringStart = OffsetDateTime.now();
+		String stringConcat = "";
 
-		Map<String, String> map = new HashMap<>();
-		
-		value = value.replace("{", "").replace("}", "").replace("\"", "");
-		var valueArr = value.split(",");
-		for (var v: valueArr) {
-			var keyValue = v.split(":");
-			map.put(keyValue[0], keyValue[1]);
+		for (int i = 0; i < 400_000; i++) {
+			stringConcat += i; // essa concatenação é muito custosa, pois a cada concatenação é criado um novo
+								// objeto String, o que consome muita memória e tempo de processamento, não
+								// sendo a forma mais indicada.
 		}
-		
-		System.out.println(map);
+		var stringEnd = OffsetDateTime.now();
+		System.out.printf("String concatenada: %s \n", getInterval(stringStart, stringEnd));
+
+		// essas são as formas recomendadas para concatenar strings em Java, pois o
+		// StringBuilder é uma classe mutável que permite modificar o conteúdo sem criar
+		// novos objetos, o que é mais eficiente em termos de memória e desempenho.Vale
+		// ressaltar que semrpre é bom fazer uma análise de mercado
+
+		var BuilderStart = OffsetDateTime.now();
+		var stringBuilder = new StringBuilder(); // singlethreads
+		for (int i = 0; i < 400_000; i++) {
+			stringBuilder.append(i);
+		}
+		var BuilderEnd = OffsetDateTime.now();
+		System.out.printf("StringBuilder (singlethreads): %s \n",
+				getInterval(BuilderStart, BuilderEnd));
+
+		var BufferStart = OffsetDateTime.now();
+		var stringBuffer = new StringBuffer(); // multithreads
+		for (int i = 0; i < 400_000; i++) {
+			stringBuffer.append(i);
+		}
+		var BufferEnd = OffsetDateTime.now();
+
+		System.out.printf("StringBuffer (multithreads): %s \n", getInterval(BufferStart, BufferEnd));
+
+	}
+
+	private static long getInterval(OffsetDateTime BufferStart, OffsetDateTime BufferEnd) {
+		return Duration.between(BufferStart, BufferEnd).toMillis();
 	}
 }
